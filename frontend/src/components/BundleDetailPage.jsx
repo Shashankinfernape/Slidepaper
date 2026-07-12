f34afb4e60ea0ae0032d615dca8e9fd6f92f8b0d
Shashankinfernape <infernapeshashank@gmail.com>
2026-07-13 02:16:53 +0530

    Freeze download page bundle hover animations and remove save feature completely

--- frontend/src/components/BundleDetailPage.jsx
+++ frontend/src/components/BundleDetailPage.jsx
@@ -612,15 +612,6 @@ export default function BundleDetailPage({
                   <span>Share</span>
                 </button>
 
-                <button
-                  className="youtube-action-pill-btn"
-                  onClick={() => alert('Bundle saved to library!')}
-                >
-                  <svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor" style={{ marginRight: '6px' }}>
-                    <path d="M14 10H2v2h12v-2zm0-4H2v2h12V6zm4 8v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zM2 16h8v-2H2v2z" />
-                  </svg>
-                  <span>Save</span>
-                </button>
               </div>
             </div>
           </div>
